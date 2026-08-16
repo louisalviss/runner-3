df060bed8d456c9504afd08465ec22f2d6b6ee8f
louisalviss <ducduy2411@gmail.com>
2026-08-17 05:31:27 +0700

    Add Runner3 theme footer

---
+++ wordpress-site/wp-content/themes/runner3-starter/footer.php
@@ -0,0 +1,9 @@
+</main>
+<footer class="site-footer">
+  <div class="wrap">
+    &copy; <?php echo esc_html(date('Y')); ?> <?php bloginfo('name'); ?>.
+  </div>
+</footer>
+<?php wp_footer(); ?>
+</body>
+</html>
