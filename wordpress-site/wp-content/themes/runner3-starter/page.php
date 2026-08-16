937503f729e96359a3ffa5ff1f481702bdefb043
louisalviss <ducduy2411@gmail.com>
2026-08-17 05:31:42 +0700

    Add Runner3 page template

---
+++ wordpress-site/wp-content/themes/runner3-starter/page.php
@@ -0,0 +1,10 @@
+<?php get_header(); ?>
+<div class="wrap content">
+  <?php while (have_posts()) : the_post(); ?>
+    <article <?php post_class(); ?>>
+      <h1><?php the_title(); ?></h1>
+      <?php the_content(); ?>
+    </article>
+  <?php endwhile; ?>
+</div>
+<?php get_footer(); ?>
