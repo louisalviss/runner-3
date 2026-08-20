bd87afb229f9c3c6a1ff424256bd75ef308c6869
louisalviss <ducduy2411@gmail.com>
2026-08-20 08:14:03 +0700

    Route Reddit share links through FxReddit first

--- scripts/audio_library_resolve_reddit_via_curlx.py
+++ scripts/audio_library_resolve_reddit_via_curlx.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
-# trigger: reddit-unfurl-v1-pr
-from audio_library_resolve_reddit_via_unfurl import main
+# trigger: reddit-rxddit-v1-pr
+from audio_library_resolve_reddit_via_rxddit import main
 
 if __name__ == '__main__':
     main()
