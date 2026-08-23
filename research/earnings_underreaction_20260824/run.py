a2b968982c904911cba71c16e080a91383e3460f
louisalviss <ducduy2411@gmail.com>
2026-08-24 06:50:08 +0700

    fix: use reachable earnings dataset URL

---
+++ research/earnings_underreaction_20260824/run.py
@@ -0,0 +1,4 @@
+import backtest
+
+backtest.EARNINGS_URL = "https://raw.githubusercontent.com/wuddup-02120/HistoricalEarningsData/master/HistoricalEarningsData/data/aggregated_earnings_data_webscraped.csv"
+backtest.main()
