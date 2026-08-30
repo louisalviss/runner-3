734032e7ec6c74e431748c46e9521960ced46dcf
louisalviss <ducduy2411@gmail.com>
2026-08-31 04:25:03 +0700

    fix(reader): activate v14 media ID sync

--- cloudflare/runner3-core/artifact-library-reader-v7-github-audio-entry.js
+++ cloudflare/runner3-core/artifact-library-reader-v7-github-audio-entry.js
@@ -1,4 +1,4 @@
-import app from "./artifact-library-reader-v13-media-id-sync-entry.js";
+import app from "./artifact-library-reader-v14-media-id-all-entry.js";
 import ebookAudio from "./src/ebook-reader-audio.js";
 
 const ROBOTS = "noindex, nofollow, noarchive, nosnippet, noimageindex";
