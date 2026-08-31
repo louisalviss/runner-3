66291521d4de9e279999646e037537df91014760
louisalviss <ducduy2411@gmail.com>
2026-09-01 05:22:30 +0700

    export DOM timing segment builder

--- cloudflare/runner3-core/reader-audio-core/index.js
+++ cloudflare/runner3-core/reader-audio-core/index.js
@@ -3,4 +3,5 @@ export { PositionMapper } from './position-mapper.js';
 export { ReaderFollower } from './reader-follower.js';
 export { PlaybackQueue } from './playback-queue.js';
 export { ReaderAudioAdapter } from './reader-audio-adapter.js';
+export { DomSegmentBuilder, normalizeReaderText, tokenizeReaderText } from './dom-segment-builder.js';
 export { normalizePlaybackState, PLAYBACK_STATE_VERSION } from './state-contract.js';
