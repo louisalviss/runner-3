19fd5ee26adb53c9d2b58738adb5e87cc810a019
louisalviss <ducduy2411@gmail.com>
2026-08-31 09:46:38 +0700

    wire Reader v24 matching-frame resume

--- cloudflare/runner3-core/artifact-library-reader-v7-github-audio-entry.js
+++ cloudflare/runner3-core/artifact-library-reader-v7-github-audio-entry.js
@@ -1,4 +1,4 @@
-import app from "./artifact-library-reader-v23-resume-cfi-id-entry.js";
+import app from "./artifact-library-reader-v24-resume-wait-match-entry.js";
 import ebookAudio from "./src/ebook-reader-audio.js";
 
 const ROBOTS = "noindex, nofollow, noarchive, nosnippet, noimageindex";
