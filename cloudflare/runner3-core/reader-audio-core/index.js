d544a241fb56b30ae825f4c1b94396beb0de989c
louisalviss <ducduy2411@gmail.com>
2026-08-31 23:29:32 +0700

    feat(reader-core): export isolated adapter

--- cloudflare/runner3-core/reader-audio-core/index.js
+++ cloudflare/runner3-core/reader-audio-core/index.js
@@ -2,4 +2,5 @@ export { AudioController } from './audio-controller.js';
 export { PositionMapper } from './position-mapper.js';
 export { ReaderFollower } from './reader-follower.js';
 export { PlaybackQueue } from './playback-queue.js';
+export { ReaderAudioAdapter } from './reader-audio-adapter.js';
 export { normalizePlaybackState, PLAYBACK_STATE_VERSION } from './state-contract.js';
