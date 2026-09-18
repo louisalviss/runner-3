72f4474be81b70250fa58093dc34d18a76bb56f3
root <root@runner-vps1.tail70ee48.ts.net>
2026-09-18 18:41:29 +0100

    Add Webnovel.vn and TruyenYY candidates

---
+++ vbook/vn-sites/truyenyy/src/home.js
@@ -0,0 +1,1 @@
+function execute(){return Response.success([{title:'Mới cập nhật',input:'https://truyenyy.co/',script:'gen.js'},{title:'Truyện dịch',input:'https://truyenyy.co/truyen-dich-yy',script:'gen.js'}]);}
---
+++ vbook/vn-sites/webnovelvn/src/home.js
@@ -0,0 +1,1 @@
+function execute(){return Response.success([{title:'Khám phá',input:'https://webnovel.vn/',script:'gen.js'},{title:'Mới cập nhật',input:'https://webnovel.vn/all/',script:'gen.js'}]);}
